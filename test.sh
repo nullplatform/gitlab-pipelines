109272be00abf75a96e3df13375017d074bcb372
Pablo Ilundain <pabloilundain@gmail.com>
2023-08-04 20:58:44 +0000

    fix

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-RESPONSE=$(curl -s "$API_URL/v1/employees")
-MESSAGE=$(echo $RESPONSE | jq -r '.message')
-export MESSAGE=$MESSAGE
-export PABLO="PABLO1"
-export PABLO2="PABLO2"
+RESPONSE=$(curl -s "https://dummy.restapiexample.com/api/v1/employees")
+export MESSAGE=$(echo $RESPONSE | jq -r '.message')

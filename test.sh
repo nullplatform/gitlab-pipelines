b3443e200887b426eb7e8a4a3de34345e3ade149
Pablo Ilundain <pabloilundain@gmail.com>
2023-08-04 21:40:47 +0000

    fix

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-RESPONSE=$(curl -s "https://dummy.restapiexample.com/api/v1/employee/$1")
+RESPONSE=$(curl -s "https://dummy.restapiexample.com/api/v1/employee/14")
 export NAME=$(echo $RESPONSE | jq -r '.data.employee_name')
 export AGE=$(echo $RESPONSE | jq -r '.data.employee_age')
